10a0c7e8ec2ff543cf76f6c26ddc5946566375b3
nomuus <927151+nomuus@users.noreply.github.com>
2023-01-04 15:10:40 -0600

    improve function name exclusions for Word and Excel

--- obfuscator/modules/formatters/variable_names_formatter.py
+++ obfuscator/modules/formatters/variable_names_formatter.py
@@ -8,9 +8,18 @@ class VariableNamesFormatter(Formatter):
     def __init__(self, rand):
         super().__init__()
         self.rand = rand
+        self.excluded_function_names = [
+            'AutoOpen',  # Word
+            'Workbook_Open',  # Excel
+        ]
 
     def format(self, tokensource, outfile):
         for ttype, value in tokensource:
+            if ttype == Token.Name.Function \
+            and value in self.excluded_function_names:
+                outfile.write(value)
+                continue
+            
             if (ttype == Token.Name or ttype == Token.Name.Function) and value in self.rand and value != "AutoOpen":
                 outfile.write(self.rand[value])
             else:
